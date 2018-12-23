117dab42069641f3844ebb9fa112c4def3f0762f
iromli <isman.firmansyah@gmail.com>
2018-12-23 14:39:03 +0700

    add kubernetes secret adapter

--- scripts/gluulib.py
+++ scripts/gluulib.py
@@ -1,3 +1,4 @@
+import base64
 import json
 import logging
 import os
@@ -335,6 +336,11 @@ class BaseSecret(object):
         """
         raise NotImplementedError
 
+    def _prepare_value(self, value):
+        if not isinstance(value, (six.string_types, six.binary_type)):
+            value = json.dumps(value)
+        return value
+
 
 class VaultSecret(BaseSecret):
     def __init__(self):
@@ -428,6 +434,90 @@ class VaultSecret(BaseSecret):
         self.client.write("{}/{}".format(self.prefix, key), **val)
 
 
+class KubernetesSecret(BaseSecret):
+    def __init__(self):
+        self.settings = {
+            k: v for k, v in os.environ.iteritems()
+            if k.isupper() and k.startswith("GLUU_SECRET_KUBERNETES_")
+        }
+        self.settings.setdefault(
+            "GLUU_SECRET_KUBERNETES_NAMESPACE",
+            "default",
+        )
+        self.settings.setdefault(
+            "GLUU_SECRET_KUBERNETES_SECRET",
+            "gluu",
+        )
+        self.settings.setdefault(
+            "GLUU_SECRET_KUBERNETES_USE_KUBE_CONFIG",
+            False
+        )
+
+        if as_boolean(self.settings["GLUU_SECRET_KUBERNETES_USE_KUBE_CONFIG"]):
+            kubernetes.config.load_kube_config()
+        else:
+            kubernetes.config.load_incluster_config()
+        self.client = kubernetes.client.CoreV1Api()
+        self.name_exists = False
+
+    def get(self, key, default=None):
+        result = self.all()
+        if key in result:
+            return base64.b64decode(result[key])
+        return default
+
+    def _prepare_secret(self):
+        # create a secret name if not exist
+        if not self.name_exists:
+            try:
+                self.client.read_namespaced_secret(
+                    self.settings["GLUU_SECRET_KUBERNETES_SECRET"],
+                    self.settings["GLUU_SECRET_KUBERNETES_NAMESPACE"])
+                self.name_exists = True
+            except kubernetes.client.rest.ApiException as exc:
+                if exc.status == 404:
+                    # create the secrets name
+                    body = {
+                        "kind": "Secret",
+                        "apiVersion": "v1",
+                        "metadata": {
+                            "name": self.settings["GLUU_SECRET_KUBERNETES_SECRET"],
+                        },
+                        "data": {},
+                    }
+                    created = self.client.create_namespaced_secret(
+                        self.settings["GLUU_SECRET_KUBERNETES_NAMESPACE"],
+                        body)
+                    if created:
+                        self.name_exists = True
+                else:
+                    raise
+
+    def set(self, key, value):
+        self._prepare_secret()
+        body = {
+            "kind": "Secret",
+            "apiVersion": "v1",
+            "metadata": {
+                "name": self.settings["GLUU_SECRET_KUBERNETES_SECRET"],
+            },
+            "data": {
+                key: base64.b64encode(value),
+            }
+        }
+        return self.client.patch_namespaced_secret(
+            self.settings["GLUU_SECRET_KUBERNETES_SECRET"],
+            self.settings["GLUU_SECRET_KUBERNETES_NAMESPACE"],
+            body=body)
+
+    def all(self):
+        self._prepare_secret()
+        result = self.client.read_namespaced_secret(
+            self.settings["GLUU_SECRET_KUBERNETES_SECRET"],
+            self.settings["GLUU_SECRET_KUBERNETES_NAMESPACE"])
+        return result.data or {}
+
+
 class SecretManager(object):
     def __init__(self, config_adapter=None):
         _adapter = os.environ.get(
@@ -436,6 +526,8 @@ class SecretManager(object):
         )
         if _adapter == "vault":
             self.adapter = VaultSecret()
+        elif _adapter == "kubernetes":
+            self.adapter = KubernetesSecret()
         else:
             self.adapter = None
 
